09293175d44adbd7b9381b34c44a68ae185c469c
Surya Saputra <17169567+suryasaputra2016@users.noreply.github.com>
2025-01-13 15:41:38 +0700

    add cron jon

--- server/main.go
+++ server/main.go
@@ -4,8 +4,10 @@ import (
 	"log"
 	"net"
 
-	"github.com/suryasaputra2016/phase-3-graded-challenge-2/proto/pb"
+	"github.com/robfig/cron/v3"
+	"github.com/suryasaputra2016/phase-3-graded-challenge-2/server/proto/pb"
 	"github.com/suryasaputra2016/phase-3-graded-challenge-2/server/servers"
+	"github.com/suryasaputra2016/phase-3-graded-challenge-2/server/utils"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
@@ -22,6 +24,12 @@ func main() {
 	pb.RegisterBookServiceServer(grpcServer, &servers.Server{})
 
 	reflection.Register(grpcServer)
+
+	// cron job
+	c := cron.New()
+	c.Start()
+	s := &utils.Server{}
+	s.StartSchedulerJob()
 
 	log.Println("User server is running on port :50051")
 	if err := grpcServer.Serve(lis); err != nil {
--- server/utils/cronjob.go
+++ server/utils/cronjob.go
@@ -4,22 +4,23 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"net/http"
 	"time"
 
-	"github.com/labstack/echo/v4"
 	"github.com/robfig/cron/v3"
+	"github.com/suryasaputra2016/phase-3-graded-challenge-2/server/configs"
+	"github.com/suryasaputra2016/phase-3-graded-challenge-2/server/entities"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
-type server struct{}
+type Server struct{}
 
-func (s *server) StartSchedulerJob() error {
+func (s *Server) StartSchedulerJob() error {
 	c := cron.New()
 
 	_, err := c.AddFunc("0 0 * * *", func() {
 		_, cancel := context.WithTimeout(context.Background(), time.Second*10)
 		defer cancel()
-		fmt.Println("Rest job executed")
+		updateStatus()
 	})
 	if err != nil {
 		log.Printf("Error adding cron job: %v", err)
@@ -33,9 +34,51 @@ func (s *server) StartSchedulerJob() error {
 	return nil
 }
 
-func (s *server) startScheduledJobHandler(c echo.Context) error {
-	if err := s.StartSchedulerJob(); err != nil {
-		return c.String(http.StatusInternalServerError, fmt.Sprintf("failed to start scheduled job: %v", err))
+func updateStatus() error {
+	// get collection
+	collection, err := configs.BookDatabase(context.Background())
+	if err != nil {
+		return err
 	}
-	return c.String(http.StatusOK, "Scheduled job started successfully")
+
+	// data container
+	var datas []entities.Book
+
+	// get cursor
+	cursor, err := collection.Find(context.Background(), bson.D{{}})
+	if err != nil {
+		return err
+	}
+
+	// loop Cursor and get all data
+	for cursor.Next(context.Background()) {
+		var data entities.Book
+		if err := cursor.Decode(&data); err != nil {
+			return err
+		}
+		datas = append(datas, data)
+	}
+	if err := cursor.Err(); err != nil {
+		return err
+	}
+
+	// update data version
+	for _, data := range datas {
+		if data.Status != "overdue" {
+			data.Status = "overdue"
+
+			_, err = collection.UpdateOne(
+				context.Background(),
+				bson.M{"_id": data.ID},
+				bson.M{"$set": data},
+			)
+			if err != nil {
+				return err
+			}
+		}
+
+	}
+
+	fmt.Println("book status updated")
+	return nil
 }
